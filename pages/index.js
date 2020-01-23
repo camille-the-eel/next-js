206b508294f6e0166d9ed505078ea209840630bb
Camille <c.hughes64@gmail.com>
2020-01-23 14:09:41 -0500

    next-js walkthrough

---
+++ pages/index.js
@@ -0,0 +1,12 @@
+import Link from 'next/link';
+
+const Index = () => (
+    <div>
+        <Link href="/about">
+            <a title="About Page">abouts.</a>
+        </Link>
+            <p>meep.</p>
+    </div>
+);
+
+export default Index;
